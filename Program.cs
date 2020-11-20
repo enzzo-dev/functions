90fa81dd2fa359affddd3ea8db6b9a96bc0ad350
Enzzo Lima Alves <enzzolimaalves2013@gmail.com>
2020-11-20 19:22:52 -0300

    Exercicio feito -a média geral da sala

--- Program.cs
+++ Program.cs
@@ -7,35 +7,33 @@ namespace functions
         static void Main(string[] args)
         {
 
-            float[] nota1 = new float[4];
-            float[] nota2 = new float[4];
-            float[] nota3 = new float[4];
-            float[] nota4 = new float[4];
-            float[] mediaGeral = new float[10];
-            float[] mediaDoAluno = new float[5];
-
+            float[] nota1 = new float[10];
+            float[] nota2 = new float[10];
+            float[] nota3 = new float[10];
+            float[] nota4 = new float[10];
+            float[] medias = new float[10];
             string[] alunos = new string[10];
 
-            for(var i = 0 ; i < 10 ; i++)
-            {
-                Console.WriteLine($"Digite o nome do {i + 1}° aluno: ");
-                alunos[i] = Console.ReadLine();
-            }
+          
 
-            for(var i2 = 0 ; i2 < alunos.Length ; i2++)
+                for(var i2 = 1 ; i2 < 10; i2++)
             {
                 Console.WriteLine($"Digite as 4 notas do(a) aluno(a): {alunos[i2]}");
-                nota1[i2] = float.Parse(Console.ReadLine());
-                nota2[i2] = float.Parse(Console.ReadLine());
-                nota3[i2] = float.Parse(Console.ReadLine());
-                nota4[i2] = float.Parse(Console.ReadLine());
-            }
+                alunos[i2] = Console.ReadLine();
+                Console.Write($"Digite a {i2}° nota:"); nota1[i2] = float.Parse(Console.ReadLine());
+                Console.Write($"Digite a {i2}° nota:"); nota2[i2] = float.Parse(Console.ReadLine());
+                Console.Write($"Digite a {i2}° nota:"); nota3[i2] = float.Parse(Console.ReadLine());
+                Console.Write($"Digite a {i2}° nota:"); nota4[i2] = float.Parse(Console.ReadLine());
 
-            for(var count = 0 ; count < 4 ; count++)
-            {
-                mediaGeral[count] = nota1[count] + nota2[count] + nota3[count] + nota4[count] / 4;
-                System.Console.WriteLine($"A média geral da turma é: {mediaGeral} ");
+                medias[i2] = (nota1[i2] + nota2[i2] + nota3[i2] + nota4[i2])/4;
+                System.Console.WriteLine($"A Média do aluno {alunos[i2]} é: {medias[i2]}");
+
+                System.Console.WriteLine("------------------------Cadastrar novo aluno-------------------------");
+                
+               
+            }
+           
             }
         }
-    }
-}
+ }
+
